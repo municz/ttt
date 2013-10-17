854e747549381680a18c80d5f92258335a657e39
maros-klimovsky <395858@mail.muni.cz>
2013-10-17 11:02:39 +0200

    Added tests

--- test/tictactoe_test.rb
+++ test/tictactoe_test.rb
@@ -1,1 +1,18 @@
 require 'test_helper'
+
+describe TicTacToe do
+	it "X wins" do
+	t1 = TicTacToe.new([["X","_","O"],
+						["_","X","O"],
+						["_","_","X"]
+						])
+	t1.winner.must_equal "X"			
+	end
+	it "O wins" do
+	t1 = TicTacToe.new([["O","O","O"],
+						["_","X","O"],
+						["X","_","X"]
+						])
+	t1.winner.must_equal "O"			
+	end
+end
