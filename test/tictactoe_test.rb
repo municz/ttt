0dc17557963caf76e44a6c9edc270310e72eae58
Monika <mon.kostolna@gmail.com>
2013-10-17 11:03:02 +0200

    test_help1

--- test/tictactoe_test.rb
+++ test/tictactoe_test.rb
@@ -1,1 +1,16 @@
 require 'test_helper'
+
+describe TicTacToe do
+	describe "X wins horizontally" do
+		subject do
+			TicTacToe.new(TicTacToe.new([["X", "X", "X"],
+                    			             ["O", "O", "_"],
+                    				     ["O", "_","_"]])
+		end
+		it "X must be the winner" do
+			subject.winner.must_equal "X"
+		end
+	end
+end
+
+
