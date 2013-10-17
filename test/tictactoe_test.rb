a92d9f61a482a5813353bf52172d5971d513baff
Petr Suchy <suchar.15@seznam.cz>
2013-10-17 11:03:28 +0200

    [ADD] Tests for TicTacToe

--- test/tictactoe_test.rb
+++ test/tictactoe_test.rb
@@ -1,1 +1,45 @@
 require 'test_helper'
+
+describe TicTacToe do
+	
+	one = TicTacToe.new([["X", "X", "X"],
+                     	 ["O", "O", "_"],
+                     	 ["O", "_", "_"]])
+
+
+	two = TicTacToe.new([["O", "X", "X"],
+                     	 ["O", "O", "_"],
+                     	 ["O", "_", "_"]])
+
+	it "size must_equal to 3" do
+		one.size.must_equal 3
+		two.size.must_equal 3
+	end
+
+	it "winner is as expected" do
+		one.winner.must_equal "X"
+		two.winner.must_equal "O"
+	end
+
+	it "in row is true" do
+		one.in_row?([["X", "X", "X"],
+                     ["O", "O", "_"],
+                     ["O", "_", "_"]],"X").must_equal true
+		two.in_row?([["O", "X", "X"],
+                     ["O", "O", "_"],
+                     ["O", "_", "_"]],"X").must_equal false
+	end
+
+	it "in coll is false" do
+		one.in_coll?([["X", "X", "X"],
+                      ["O", "O", "_"],
+                      ["O", "_", "_"]],"X").must_equal false
+	end
+
+	it "in diag is false" do
+		one.in_diag?([["X", "X", "X"],
+                      ["O", "O", "_"],
+                      ["O", "_", "_"]],"X").must_equal false
+	end
+ 
+end
