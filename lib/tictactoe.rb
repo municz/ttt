7e40a875448c50b7b3ddc993ca7eaa47e33f713f
Viktor Hrtánek <viktor.hrtanek@gmail.com>
2013-10-17 11:26:09 +0200

    my change

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -23,7 +23,7 @@ class TicTacToe
   end
 
   def in_diag?(config, player)
-    diag_indexes = [(0...size).zip((0...size))]
+    diag_indexes = [(0...size).to_a.zip((0...size).to_a)]
     diag_indexes << (0...size).to_a.reverse.zip((0...size))
     diagonals = diag_indexes.map {|indexes| indexes.map { |x, y| config[x][y] } }
     in_row?(diagonals, player)
