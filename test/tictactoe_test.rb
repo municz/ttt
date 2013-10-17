b89942fae82a1d9bf45d8e97f667610b226e9909
Jan Vyhnanek <johan.vyhnanek@seznam.cz>
2013-10-17 11:03:05 +0200

    Zmena

--- test/tictactoe_test.rb
+++ test/tictactoe_test.rb
@@ -1,1 +1,36 @@
 require 'test_helper'
+
+describe TicTacToe do
+
+	describe "X wins horizontally" do
+		subject do
+			TicTacToe.new([["X", "X", "X"],
+                     	   ["O", "O", "_"],
+                     	   ["O", "_", "_"]])
+		end
+
+		it "X must be the winner" do
+ 			subject.winner.must_equal "X"
+		end
+	end
+
+	describe "X wins vertically" do
+		subject do
+			TicTacToe.new([["X", "O", "O"],
+                     	   ["X", "O", "_"],
+                     	   ["X", "_", "_"]])
+		end
+
+		it "X must be the winner" do
+ 			subject.winner.must_equal "X"
+		end
+
+		it "size must be three" do 
+			ttt.size.must_equal 3
+		end
+	end
+
+	
+
+	
+end
