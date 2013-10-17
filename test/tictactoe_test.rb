4f350fc658eca05a0e02cc08a01a577a6bc1d6bc
Jan Vyhnanek <johan.vyhnanek@seznam.cz>
2013-10-17 11:14:49 +0200

    tests 100%

--- test/tictactoe_test.rb
+++ test/tictactoe_test.rb
@@ -12,25 +12,59 @@ describe TicTacToe do
 		it "X must be the winner" do
  			subject.winner.must_equal "X"
 		end
+
+		it "X must be in row" do
+ 			subject.in_row?([["X", "X", "X"],
+                     	   ["O", "O", "_"],
+                     	   ["O", "_", "_"]], "X").must_equal true
+		end
 	end
 
-	describe "X wins vertically" do
+	describe "O wins vertically" do
 		subject do
-			TicTacToe.new([["X", "O", "O"],
-                     	   ["X", "O", "_"],
-                     	   ["X", "_", "_"]])
+			TicTacToe.new([["O", "X", "O"],
+                     	   ["O", "X", "_"],
+                     	   ["O", "_", "_"]])
 		end
 
-		it "X must be the winner" do
- 			subject.winner.must_equal "X"
+		it "O must be the winner" do
+ 			subject.winner.must_equal "O"
+		end
+
+		it "X must be in col" do
+ 			subject.in_coll?([["X", "O", "O"],
+                     	   ["X", "O", "_"],
+                     	   ["X", "_", "_"]], "X").must_equal true
 		end
 
 		it "size must be three" do 
-			ttt.size.must_equal 3
+			subject.size.must_equal 3
 		end
 	end
 
-	
+	describe "X wins diagonally" do
+		subject do
+			TicTacToe.new([["X", "O", "O"],
+                     	   ["O", "X", "_"],
+                     	   ["O", "_", "X"]])
+		end
 
-	
+		it "X must be in col" do
+ 			subject.in_diag?([["X", "O", "O"],
+                     	   ["O", "X", "_"],
+                     	   ["O", "_", "X"]], "X").must_equal true
+		end
+	end
+
+	describe "no winner" do
+		subject do
+			TicTacToe.new([["X", "O", "O"],
+                     	   ["O", "_", "_"],
+                     	   ["O", "_", "X"]])
+		end
+
+		it "no winner" do
+ 			subject.winner.must_equal nil
+		end
+	end
 end
